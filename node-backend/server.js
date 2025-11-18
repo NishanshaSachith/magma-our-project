2bf2b8b9ba3886cbf5423465fb893ab033a617f9
NishanshaSachith <nishanshasachith19@gmail.com>
2025-11-18 22:11:45 +0530

    fix 4

--- node-backend/server.js
+++ node-backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
-import { fileURLToPath } from "url";
+const { fileURLToPath } = require('url'); 
 
 dotenv.config();
 
